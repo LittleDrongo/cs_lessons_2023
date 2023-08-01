52d9f2d3e7eb6a7d144fdd8a7466f989a28de137
Fomin Alexey <fomin.alexey@outlook.com>
2023-08-01 22:17:54 +0300

    added task_5

---
+++ task_05/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ведите натуральное число N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.Write("Введено некорректное число.");
+}
+else
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count += 1;
+    }
+}
