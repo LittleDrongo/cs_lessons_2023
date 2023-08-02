43b4dc529759c7e9e2b24ee853ea76184f453b97
Fomin Alexey <fomin.alexey@outlook.com>
2023-08-02 19:09:46 +0300

    task_08 done

--- task_08/Program.cs
+++ task_08/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите натуральное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+Console.WriteLine("------------------------");
+
+while (count < number)
+{
+    count = count + 1;
+    if (count % 2 == 0)
+    {
+        Console.Write(count);
+        if (count < (number-1))
+        {
+            Console.Write(", ");
+        }
+    }
+}
