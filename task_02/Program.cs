64b71a1a1c049755bf4a6c2caad431dd27725e2f
Fomin Alexey <fomin.alexey@outlook.com>
2023-08-02 17:57:42 +0300

    added task_02

---
+++ task_02/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введита второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+
+if (numberOne > numberTwo)
+{
+    int maxNumber = numberOne;
+    int minNumber = numberTwo;
+    Console.WriteLine($"Большое число: {maxNumber}, меньшее число: {minNumber}");
+} else
+{
+    int maxNumber = numberTwo;
+    int minNumber = numberOne;
+    Console.WriteLine($"Большое число: {maxNumber}, меньшее число: {minNumber}");
+}
+
