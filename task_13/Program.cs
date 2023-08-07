ce84ae9295d0fe347af51d1daa719024bdd3d626
Fomin Alexey <fomin.alexey@outlook.com>
2023-08-07 12:13:29 +0300

    finished task_13

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -5,3 +5,26 @@
 
 //зациклить делением (!)
 //Получить трёхзначное число в цикле
+
+Console.WriteLine("Введите любое число от 100 до 2147483647");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Ошибка ввода, число должно быть трёхзначным и более");
+}
+else
+{
+    int resultNumber = FindThirdNumber(number);
+    Console.WriteLine(resultNumber);
+}
+
+int FindThirdNumber(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    return num;
+}
