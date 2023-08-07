c61abacb4a58956ea6eda84b57438d3c74002232
Fomin Alexey <fomin.alexey@outlook.com>
2023-08-07 12:56:50 +0300

    task_15 finished

--- task_15/Program.cs
+++ task_15/Program.cs
@@ -2,3 +2,18 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+Console.WriteLine("День недели в формате 1 - 7");
+int day = Convert.ToInt32(Console.ReadLine());
+string isWeekend = DayWeek(day);
+Console.WriteLine(isWeekend);
+string DayWeek (int num)
+{
+    if (num > 0 & num < 8)
+    {
+        return num > 5 ? $"{num}-й день недели — выходной" : $"{num}-й день недели — будень";
+    }
+    else 
+    {
+        return "Ошибка: неверно введен день недели, введите значение в диапазоне 1-7.";
+    }
+}
