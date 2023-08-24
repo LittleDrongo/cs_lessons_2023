c0132238ace9797e25e2e163576da24ab11a6268
Fomin Alexey <fomin.alexey@outlook.com>
2023-08-24 20:07:23 +0300

    task_23 done

--- task_23/Program.cs
+++ task_23/Program.cs
@@ -2,3 +2,16 @@
 // Пример:
 // 3 -> 1,8,27
 // 5 -> 1,8,27,64,125
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CubesNumber (number);
+
+void CubesNumber (int num)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Таблица кубов числа: {num}");
+    for (int i = 1; i-1 < num; i++)
+    {
+        Console.WriteLine(i*i*i);
+    } 
+}
