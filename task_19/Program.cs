6d34b8b01f833016b58969305ac53db629fba07a
Fomin Alexey <fomin.alexey@outlook.com>
2023-08-24 21:33:16 +0300

    task_19 done

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -2,3 +2,35 @@
 // Пример 1421 -> нет
 // Пример 23432 -> да
 // Пример 12821 -> да
+
+StartCheckPalindrom();
+
+void StartCheckPalindrom()
+{
+    Console.WriteLine ("Введите пятизначное число");
+    int fiveDigitNumber = Convert.ToInt32(Console.ReadLine());
+    CheckPalindrom(fiveDigitNumber);
+}
+
+
+bool CheckPalindrom(int num)
+{
+    if (num < 9999 || num > 10000)
+    {
+        if (num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10)
+        {
+            Console.WriteLine("Отладочное сообщение: Истина");
+            return true;            
+        }
+        else
+        {
+            Console.WriteLine("Отладочное сообщение: Ложь");
+            return false;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Отладочное сообщение: Ложь");
+        return false;
+    }
+}
