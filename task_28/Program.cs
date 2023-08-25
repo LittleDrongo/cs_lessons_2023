df872604b005ca6cce9797ab98e79142fb3315f5
Fomin Alexey <fomin.alexey@outlook.com>
2023-08-25 22:10:30 +0300

    added tasks 28, 30

---
+++ task_28/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int MultiplicationNums(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            mult *= i;
+        }
+    }
+    return mult;
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine($"{number} -> не корректное чило");
+}
+
+else
+{
+    int multNums = MultiplicationNums(number);
+    Console.WriteLine($"{multNums} -> произведения чисел от 1 до {number}");
+}
